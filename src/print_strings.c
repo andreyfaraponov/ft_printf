c5caf9953f5ae090b72c898c13c56f48dcb144a6
Andrew Faraponov <andrey.faraponov@gmail.com>
2018-01-06 22:29:33 +0200

    S dogs handle

--- src/print_strings.c
+++ src/print_strings.c
@@ -6,7 +6,7 @@
 /*   By: afarapon <afarapon@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/04 23:15:07 by afarapon          #+#    #+#             */
-/*   Updated: 2018/01/06 19:56:18 by afarapon         ###   ########.fr       */
+/*   Updated: 2018/01/06 22:28:48 by afarapon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,20 +46,23 @@ char		*ft_print_strings(char **f, t_all_flags all_flags, va_list list)
 	char			*temp;
 
 	str = 0;
-	if (**f == 's')
+	if (**f == 'S' && MB_CUR_MAX == 4)
+	{
+		arr = (unsigned int*)va_arg(list, int*);
+		if (arr == NULL)
+			str = "(null)";
+		else
+			str = ft_create_ustring(arr, ft_strnew(0));
+		temp = str;
+		str = ft_make_width_str(str, all_flags, 0);
+		free(temp);
+	}
+	else
 	{
 		str = va_arg(list, char*);
 		if (str == NULL)
 			str = "(null)";
 		str = ft_make_width_str(str, all_flags, 0);
-	}
-	else
-	{
-		arr = (unsigned int*)va_arg(list, int*);
-		str = ft_create_ustring(arr, ft_strnew(0));
-		temp = str;
-		str = ft_make_width_str(str, all_flags, 0);
-		free(temp);
 	}
 	(*f)++;
 	return (str);
